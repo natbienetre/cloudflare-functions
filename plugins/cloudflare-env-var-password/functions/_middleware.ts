d08493a445737fc228a747ff38b2d09003723c09
Pierre PÉRONNET <pierre.peronnet@datadoghq.com>
2023-08-15 17:13:08 +0200

    fix(env-var-password) Auth requires static type on constructor
    Signed-off-by: Pierre PÉRONNET <pierre.peronnet@datadoghq.com>

--- plugins/cloudflare-env-var-password/functions/_middleware.ts
+++ plugins/cloudflare-env-var-password/functions/_middleware.ts
@@ -1,11 +1,13 @@
 import autoSession from '@natbienetre/cloudflare-auto-session'
+
 import type { PluginArgs } from '../src/types'
 import { Auth } from '../src/authenticator'
 import { withDefaults } from '../src/args'
 
-export const onRequest = (context: EventPluginContext<any, any, any, PluginArgs>): Response => {
+export const onRequest = (context: EventPluginContext<Record<string, string | undefined>, any, any, PluginArgs>): Response => {
   const { passwordEncodingMethod, passwordFieldName, getEnvVarName } = withDefaults(context.pluginArgs)
-  const auth = new Auth(context, getEnvVarName, passwordEncodingMethod, passwordFieldName)
+
+  const auth = new Auth(context.request, context.env, getEnvVarName(context), passwordEncodingMethod, passwordFieldName)
 
   return autoSession({
     secret: context.env.SECRET,
